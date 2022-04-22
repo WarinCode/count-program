275b44136f86f8fed8c04fdbea5cff5730dd75a7
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-04-22 19:12:02 +0700

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -20,7 +20,7 @@ function Footer(){
                     <div class="container">
                         <footer class="py-3 my-4">
                             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                                <li class="nav-item"><a href="mailto:warinsaipanya471@protonmai.com" class="nav-link px-2 text-muted"><i class="ri-mail-unread-fill"></i></a></li>
+                                <li class="nav-item"><a href="mailto:warinsaipanya471@protonmail.com" class="nav-link px-2 text-muted"><i class="ri-mail-unread-fill"></i></a></li>
                                 <li class="nav-item"><a href="https://follow-my-ig.web.app/" class="nav-link px-2 text-muted"><i class="ri-instagram-fill"></i></a></li>
                                 <li class="nav-item"><a href="https://add-friends-7eead.web.app/" class="nav-link px-2 text-muted"><i class="ri-line-fill"></i></a></li>
                                 <li class="nav-item"><a href="https://add-friend-github.web.app/" class="nav-link px-2 text-muted"><i class="ri-github-fill"></i></a></li>
