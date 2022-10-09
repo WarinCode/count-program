a8f7d2f739575677ff0575ce920e0d0a2fe2e60a
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-10-09 11:28:46 +0700

    Update footer.jsx

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,37 +1,31 @@
-
-// react
+// import react
 import React from "react";
 
-// style
-import '../style/App.css'
-import {marginbottom} from '../style/style'
+// import style
+import '../style/App.css';
+import { marginbottom } from '../style/style';
 
-function Footer(){
-
-    return(
+const Footer = () => {
+    return (
         <>
-
             <section style={marginbottom}></section>
-
-
-                {/* Credit: bootstrap5  (source: https://getbootstrap.com/docs/5.0/examples/) */}
+            {/* Credit: bootstrap5  (source: https://getbootstrap.com/docs/5.0/examples/) */}
                 <footer id="footer" class="footer">
                     <div class="container">
                         <footer class="py-3 my-4">
                             <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                                <li class="nav-item"><a href="mailto:warinsaipanya471@protonmail.com" class="nav-link px-2 text-muted"><i class="ri-mail-unread-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://follow-my-ig.web.app/" class="nav-link px-2 text-muted"><i class="ri-instagram-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://add-friends-7eead.web.app/" class="nav-link px-2 text-muted"><i class="ri-line-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://add-friend-github.web.app/" class="nav-link px-2 text-muted"><i class="ri-github-fill"></i></a></li>
-                                <li class="nav-item"><a href="https://join-group-discord.web.app/" class="nav-link px-2 text-muted"><i class="ri-discord-fill"></i></a></li>
+                               <li className="nav-item"><a href="mailto:warinsaipanya471@protonmail.com" className="nav-link px-2 text-dark"><i className="ri-mail-unread-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://add-friend-instagram.netlify.app/" className="nav-link px-2 text-dark"><i className="ri-instagram-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://add-friend-line.netlify.app/"   className="nav-link px-2 text-dark"><i className="ri-line-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://add-friend-github.netlify.app/"  className="nav-link px-2 text-dark"><i className="ri-github-fill"></i></a></li>
+                                <li className="nav-item"><a href="https://discord-invite-029wedw.netlify.app/"  className="nav-link px-2 text-dark"><i className="ri-discord-fill"></i></a></li>
                             </ul>
                             <p class="text-center text-dark">&copy; 2022 All Rights Reserved.</p>
                         </footer>
                     </div>
                 </footer>
-
         </>     
-    );
-};
+    )
+}
 
-export{ Footer };
+export default Footer;
