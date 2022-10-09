8ea5681276d8cada17dd1c7385ecd7923164d1d5
VarinCode <94356640+VarinCode@users.noreply.github.com>
2022-10-09 11:30:47 +0700

    Update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -1,36 +1,35 @@
+// import react
 import React from "react";
-import Swal from "sweetalert2";
 
-// img
+// import img
 import logo from '../img/logo.svg';
 
-const Navbar = ()=>{
+// import lib
+import Swal from "sweetalert2";
 
-      
-    function react(){
+const Navbar = () => {
+    function react() {
       Swal.fire({
         title: 'React-project',
         text: 'โปรเจคนี้ทำเกี่ยวกับโปรแกรมนับจำนวนและตัวเลข',
         imageUrl: logo,
         imageWidth: 400,
         imageHeight: 200,
-        imageAlt: 'Custom image',
+        imageAlt: 'Custom image'
       });
     };
   
     return (
       <>
-
-          <nav class="navbar navbar-expand navbar-dark bg-dark">
+          <nav class="navbar navbar-expand navbar-dark bg-dark" onClick={react}>
               <div class="container">
                 <img src= {logo} alt=""  width={'55px'} height= {'55px'} onClick={react} style={{cursor: 'pointer'}} />
                   <h3 className="navbar-brand mt-2" onClick={react} style={{cursor: 'pointer'}}>React-project</h3>
                       <div class="collapse navbar-collapse" id="navbarNavDropdown"></div>
               </div>
           </nav>
-
       </> 
-    );
-};
+    )
+}
 
-export{ Navbar };
+export default Navbar;
